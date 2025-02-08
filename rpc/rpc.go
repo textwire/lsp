5804be431fc70aac740fabcc31b31c7c403d903d
SerhiiCho <serhiicho@protonmail.com>
2025-02-08 13:39:42 +0200

    WIP on message decoding function

--- rpc/rpc.go
+++ rpc/rpc.go
@@ -1,9 +1,12 @@
 package rpc
 
 import (
+	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
+	"strconv"
 )
 
 func EncodeMessage(msg any) string {
@@ -13,4 +16,22 @@ func EncodeMessage(msg any) string {
 	}
 
 	return fmt.Sprintf("Content-Length: %d\r\n\r\n%s", len(content), content)
+}
+
+func DecodeMessage(msg []byte) (int, error) {
+	header, content, found := bytes.Cut(msg, []byte{'\r', '\n', '\r', '\n'})
+	if !found {
+		return 0, errors.New("Did not find a separator")
+	}
+
+	headerLabelLen := len("Content-Length: ")
+	contentLenBytes := header[headerLabelLen:]
+	contentLen, err := strconv.Atoi(string(contentLenBytes))
+	if err != nil {
+		return 0, err
+	}
+
+	_ = content
+
+	return contentLen, nil
 }
--- rpc/rpc_test.go
+++ rpc/rpc_test.go
@@ -10,6 +10,19 @@ func TestEncodeMessage(t *testing.T) {
 	expected := "Content-Length: 16\r\n\r\n{\"Testing\":true}"
 	actual := EncodeMessage(EncodingExample{Testing: true})
 	if expected != actual {
-		t.Fatalf("Expected %s, Actual: %s", expected, actual)
+		t.Fatalf("Expected: %s, Actual: %s", expected, actual)
+	}
+}
+
+func TestDecodeMessage(t *testing.T) {
+	incomingMsg := "Content-Length: 16\r\n\r\n{\"Testing\":true}"
+
+	contentLen, err := DecodeMessage([]byte(incomingMsg) )
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if contentLen != 16 {
+		t.Fatalf("Expected: 16, Actual: %d", contentLen)
 	}
 }
